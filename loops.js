0a8c72951b3d49435853933123ac9bfcc52c97a3
Section9-Togusa <corbindac@gmail.com>
2022-10-12 20:50:00 -0700

    first commit

---
+++ loops.js
@@ -0,0 +1,20 @@
+const input = "i am a genius"
+const vowels = ["a","e","i", "o", "u"]
+const resultsArray = [] 
+
+for(let i = 0; i < input.length; i++){
+    for(let j = 0; j < vowels.length; j++){
+        if(input[i] === vowels[j]){
+            resultsArray.push(input[i])
+        }
+        if(input[i] === "e"){ // this will double every e it finds. 
+            resultsArray.push(input[i]) //
+        }
+        if(input[i] === "u"){ // this will double ever u it finds. 
+            resultsArray.push(input[i])
+        }
+    }
+}
+
+const finalResults = resultsArray.join('').toUpperCase()
+console.log(finalResults)
